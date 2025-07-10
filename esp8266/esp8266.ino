45ac53779ccdc4b28d34593fea3d3a009343f443
NoIdeaForMyName <maksanty.michal@gmail.com>
2025-07-10 16:17:03 +0200

    Logs on serial output deleted and automatic reconnection to wifi after losing connection added

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -26,7 +26,6 @@ void syncTime();
 void mqttCallback(char *topic, byte *payload, unsigned int length);
 
 void setup() {
-    Serial.begin(115200);
     connectToWiFi();
     syncTime();  // X.509 validation requires synchronization time
     mqtt_client.setServer(mqtt_broker, mqtt_port);
@@ -41,26 +40,15 @@ void connectToWiFi() {
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("Connecting to WiFi...");
     }
-    Serial.println("Connected to WiFi");
 }
 
 void syncTime() {
     configTime(gmt_offset_sec, daylight_offset_sec, ntp_server);
-    Serial.print("Waiting for NTP time sync: ");
     while (time(nullptr) < 8 * 3600 * 2) {
         delay(1000);
-        Serial.print(".");
     }
-    Serial.println("Time synchronized");
     struct tm timeinfo;
-    if (getLocalTime(&timeinfo)) {
-        Serial.print("Current time: ");
-        Serial.println(asctime(&timeinfo));
-    } else {
-        Serial.println("Failed to obtain local time");
-    }
 }
 
 void connectToMQTT() {
@@ -68,34 +56,23 @@ void connectToMQTT() {
     espClient.setTrustAnchors(&serverTrustedCA);
     while (!mqtt_client.connected()) {
         String client_id = "esp8266-client-" + String(WiFi.macAddress());
-        Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
         if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-            Serial.println("Connected to MQTT broker");
             mqtt_client.subscribe(WOL_TOPIC);
         } else {
             char err_buf[128];
             espClient.getLastSSLError(err_buf, sizeof(err_buf));
-            Serial.print("Failed to connect to MQTT broker, rc=");
-            Serial.println(mqtt_client.state());
-            Serial.print("SSL error: ");
-            Serial.println(err_buf);
             delay(5000);
         }
     }
 }
 
 void mqttCallback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message received on topic: ");
-    Serial.print(topic);
-    Serial.print(": [");
     if (strcmp(topic, WOL_TOPIC) == 0) {
         char *mac_address = new char[length+1];
         for (int i = 0; i < length; i++) {
             mac_address[i] = (char) payload[i];
         }
         mac_address[length] = '\0';
-        Serial.print(mac_address);
-        Serial.print("]\n");
 
         WOL.sendMagicPacket(mac_address, WOL_PORT);
         delete[] mac_address;
@@ -103,8 +80,14 @@ void mqttCallback(char *topic, byte *payload, unsigned int length) {
 }
 
 void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        connectToWiFi();
+        syncTime();
+    }
+
     if (!mqtt_client.connected()) {
         connectToMQTT();
     }
+
     mqtt_client.loop();
 }
